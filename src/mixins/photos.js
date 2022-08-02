5e60b974f336070b7d5980f5fd63d3b9a778c605
Manuel Kasper <mk@neon1.net>
2022-08-02 13:28:55 +0200

    Move original size photos to Backblaze (S3 compatible storage)

--- src/mixins/photos.js
+++ src/mixins/photos.js
@@ -1,7 +1,11 @@
 export default {
   methods: {
     photoSrc (photo, size) {
-      return 'https://images.sotl.as/photos/' + size + '/' + photo.filename.substring(0, 2) + '/' + photo.filename
+      if (size === 'original') {
+        return 'https://sotlas.s3.eu-central-003.backblazeb2.com/' + photo.filename
+      } else {
+        return 'https://images.sotl.as/photos/' + size + '/' + photo.filename.substring(0, 2) + '/' + photo.filename
+      }
     }
   }
 }
