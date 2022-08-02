9fea21d761a4ba5ae091e15ce2bb820a75325032
Manuel Kasper <mk@neon1.net>
2022-08-02 13:57:42 +0200

    Update URL

--- src/mixins/photos.js
+++ src/mixins/photos.js
@@ -2,7 +2,7 @@ export default {
   methods: {
     photoSrc (photo, size) {
       if (size === 'original') {
-        return 'https://sotlas.s3.eu-central-003.backblazeb2.com/' + photo.filename
+        return 'https://sotlas-photos.s3.eu-central-003.backblazeb2.com/original/' + photo.filename
       } else {
         return 'https://images.sotl.as/photos/' + size + '/' + photo.filename.substring(0, 2) + '/' + photo.filename
       }
