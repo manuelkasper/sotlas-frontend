cba61ea997ca4095ec7458b664dfdf65d58cf3ae
Manuel Kasper <mk@neon1.net>
2022-10-23 18:36:17 +0100

    Remove special handling for comma as it causes problems on iOS 16

--- src/components/FrequencyInput.vue
+++ src/components/FrequencyInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-input :value="value" type="number" inputmode="decimal" lang="en_EN" step="any" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" required @keydown.native="frequencyKeydown" @input="updateValue" />
+  <b-input :value="value" type="number" inputmode="decimal" lang="en_EN" step="any" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" required @input="updateValue" />
 </template>
 
 <script>
@@ -10,13 +10,6 @@ export default {
   methods: {
     updateValue (value) {
       this.$emit('input', value)
-    },
-    frequencyKeydown (event) {
-      if (event.keyCode === 188) {
-        event.target.value += '.'
-        event.preventDefault()
-        return false
-      }
     }
   }
 }
