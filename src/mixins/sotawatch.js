e7370d89775de5038401353f75e40cf458f2d46a
Manuel Kasper <mk@neon1.net>
2024-10-28 10:36:29 +0100

    Transition to new api-db2 URLs

--- src/mixins/sotawatch.js
+++ src/mixins/sotawatch.js
@@ -4,16 +4,16 @@ export default {
   mixins: [ssoauth],
   methods: {
     postSotaWatchSpot (params) {
-      return this.axiosAuthId.post('https://api2.sota.org.uk/api/spots', params)
+      return this.axiosAuthId.post('https://api-db2.sota.org.uk/api/spots', params)
     },
     deleteSotaWatchSpot (spotId) {
-      return this.axiosAuthId.delete('https://api2.sota.org.uk/api/spots/' + spotId)
+      return this.axiosAuthId.delete('https://api-db2.sota.org.uk/api/spots/' + spotId)
     },
     postSotaWatchAlert (params) {
-      return this.axiosAuthId.post('https://api2.sota.org.uk/api/alerts', params)
+      return this.axiosAuthId.post('https://api-db2.sota.org.uk/api/alerts', params)
     },
     deleteSotaWatchAlert (alertId) {
-      return this.axiosAuthId.delete('https://api2.sota.org.uk/api/alerts/' + alertId)
+      return this.axiosAuthId.delete('https://api-db2.sota.org.uk/api/alerts/' + alertId)
     }
   }
 }
