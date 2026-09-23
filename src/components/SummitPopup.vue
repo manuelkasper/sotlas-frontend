b1c0029a24746537948804e8eac3eba6a6e9033a
Manuel Kasper <mk@neon1.net>
2026-09-23 18:52:31 +0200

    Fix invalid HTML

--- src/components/SummitPopup.vue
+++ src/components/SummitPopup.vue
@@ -8,12 +8,14 @@
       </div>
       <h2>{{ summit.name }}<span class="summitCode"><router-link :to="'/summits/' + summit.code">{{ summit.code }}</router-link></span></h2>
       <table class="summitinfo">
-        <tr><th>Altitude</th><td><AltitudeLabel :altitude="summit.altitude" /></td></tr>
-        <tr class="points"><th>Points</th><td><SummitPointsLabel :points="summit.points" :bonus="summit.bonusPoints" /></td></tr>
-        <tr><th>Activations</th><td>{{ summit.activationCount }}</td></tr>
-        <tr v-if="summit.activationDate"><th>Last activation</th><td>{{ formatActivationDate(summit.activationDate) }} (<router-link :to="makeActivatorLink(summit.activationCall)">{{ summit.activationCall }}</router-link>)</td></tr>
-        <tr v-if="lastSpot"><th>Last spot</th><td><span v-html="formatTimeDay(lastSpot.timeStamp)" />: <router-link :to="makeActivatorLink(lastSpot.activatorCallsign)">{{ lastSpot.activatorCallsign }}</router-link><span v-if="lastSpot.frequency">, {{ lastSpot.frequency }}</span> <ModeLabel :mode="lastSpot.mode" :type="lastSpot.type" /></td></tr>
-        <tr v-if="nextAlert" class="nextAlert"><th>Next alert</th><td><span v-html="formatDateTimeRelative(nextAlert.dateActivated)" />: <router-link :to="makeActivatorLink(nextAlert.activatorCallsign)">{{ nextAlert.activatorCallsign }}</router-link><div v-if="nextAlert.frequency" class="alertFrequencies">{{ nextAlert.frequency }}</div><div v-if="nextAlert.comments" class="alertComments">{{ nextAlert.comments }}</div></td></tr>
+        <tbody>
+          <tr><th>Altitude</th><td><AltitudeLabel :altitude="summit.altitude" /></td></tr>
+          <tr class="points"><th>Points</th><td><SummitPointsLabel :points="summit.points" :bonus="summit.bonusPoints" /></td></tr>
+          <tr><th>Activations</th><td>{{ summit.activationCount }}</td></tr>
+          <tr v-if="summit.activationDate"><th>Last activation</th><td>{{ formatActivationDate(summit.activationDate) }} (<router-link :to="makeActivatorLink(summit.activationCall)">{{ summit.activationCall }}</router-link>)</td></tr>
+          <tr v-if="lastSpot"><th>Last spot</th><td><span v-html="formatTimeDay(lastSpot.timeStamp)" />: <router-link :to="makeActivatorLink(lastSpot.activatorCallsign)">{{ lastSpot.activatorCallsign }}</router-link><span v-if="lastSpot.frequency">, {{ lastSpot.frequency }}</span> <ModeLabel :mode="lastSpot.mode" :type="lastSpot.type" /></td></tr>
+          <tr v-if="nextAlert" class="nextAlert"><th>Next alert</th><td><span v-html="formatDateTimeRelative(nextAlert.dateActivated)" />: <router-link :to="makeActivatorLink(nextAlert.activatorCallsign)">{{ nextAlert.activatorCallsign }}</router-link><div v-if="nextAlert.frequency" class="alertFrequencies">{{ nextAlert.frequency }}</div><div v-if="nextAlert.comments" class="alertComments">{{ nextAlert.comments }}</div></td></tr>
+        </tbody>
       </table>
       <div class="buttons">
         <b-button v-if="!minimizePopup" size="is-small" icon-left="window-close" @click="$emit('close')">Close</b-button>
--- src/views/About.vue
+++ src/views/About.vue
@@ -33,30 +33,28 @@
             The source code of SOTLAS is available on <a href="https://github.com/manuelkasper/sotlas-frontend"><font-awesome-icon :icon="['fab', 'github']" size="lg" /> GitHub</a>.
           </p>
 
-          <p>Data sourced from:
-            <ul>
-              <li><a href="https://www.sotadata.org.uk">SOTA Database</a></li>
-              <li><a href="https://sotawatch.sota.org.uk">SOTAwatch</a></li>
-              <li><a href="https://www.sotamaps.org">SOTA Maps</a></li>
-              <li><a href="https://en.wikipedia.org">Wikipedia</a></li>
-              <li><a href="http://reversebeacon.net">Reverse Beacon Network</a></li>
-            </ul>
-          </p>
+          <p>Data sourced from:</p>
+          <ul>
+            <li><a href="https://www.sotadata.org.uk">SOTA Database</a></li>
+            <li><a href="https://sotawatch.sota.org.uk">SOTAwatch</a></li>
+            <li><a href="https://www.sotamaps.org">SOTA Maps</a></li>
+            <li><a href="https://en.wikipedia.org">Wikipedia</a></li>
+            <li><a href="http://reversebeacon.net">Reverse Beacon Network</a></li>
+          </ul>
 
           <p>
             Icons by <a href="https://icons8.com/">Icons8</a>.<br />
             Icons made by <a href="https://www.flaticon.com/authors/nikita-golubev" title="Nikita Golubev">Nikita Golubev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> are licensed by <a href="https://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
           </p>
 
-          <p>Technologies used:
-            <ul>
-              <li><a href="https://maptiler.com/">MapTiler</a></li>
-              <li><a href="https://nodejs.org/">Node.js</a> server</li>
-              <li>SPA built with <a href="https://vuejs.org">Vue.js</a>, <a href="https://vuex.vuejs.org">Vuex</a> and <a href="https://router.vuejs.org">Vue Router</a></li>
-              <li><a href="https://buefy.org">Buefy</a> UI framework</li>
-              <li>WebSocket</li>
-            </ul>
-          </p>
+          <p>Technologies used:</p>
+          <ul>
+            <li><a href="https://maptiler.com/">MapTiler</a></li>
+            <li><a href="https://nodejs.org/">Node.js</a> server</li>
+            <li>SPA built with <a href="https://vuejs.org">Vue.js</a>, <a href="https://vuex.vuejs.org">Vuex</a> and <a href="https://router.vuejs.org">Vue Router</a></li>
+            <li><a href="https://buefy.org">Buefy</a> UI framework</li>
+            <li>WebSocket</li>
+          </ul>
 
           <p><strong>All information on this web site is provided “as is” without warranty of any kind. The operator of this web site makes no representations and disclaims all express, implied, and statutory warranties of any kind to any user and/or any third party, including as to accuracy, timeliness, completeness, merchantability, or fitness for any particular purpose. Use of this web site and the information contained therein is at the user’s sole risk.</strong></p>
         </div>
